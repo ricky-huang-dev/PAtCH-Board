303694c5c6c6cce457beb3aedb1dc6f34446c12a
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-11-03 10:22:17 +1300

    create css for stickynote and board

--- client/components/StickyNote.tsx
+++ client/components/StickyNote.tsx
@@ -1,7 +1,10 @@
 function StickyNote() {
   return (
     <div className="stickyNote">
-      <h1>Hello World</h1>
+      <div className="noteTitle">
+        <h1>Hello World</h1>
+      </div>
+      <div className="listItems"></div>
     </div>
   )
 }
