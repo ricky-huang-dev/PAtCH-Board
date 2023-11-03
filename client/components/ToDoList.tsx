5d503ca6a15ec1cb31bb37bbe53b9f9738b7e046
David <14233215+davidlpoole@users.noreply.github.com>
2023-11-03 13:02:47 +1300

    Add form and comments

--- client/components/ToDoList.tsx
+++ client/components/ToDoList.tsx
@@ -11,40 +11,51 @@ type Todo = {
 }
 
 function ToDoList(props: Props) {
+  // State to manage the list of todos and the input text for a new todo
   const [todos, setTodos] = useState([] as Todo[])
   const [todoText, setTodoText] = useState('')
 
-  function handleAddTodo() {
+  // Function to handle adding a new todo to the list
+  function handleAddTodo(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
     const newTodo = {
-      id: todos.length,
-      text: todoText,
-      completed: false,
+      id: todos.length, // Assign a unique id based on the length of the current todos list
+      text: todoText, // Take the text from the input
+      completed: false, // Initially set as not completed
     }
 
+    // Update the todos state with the new todo and reset the input text
     setTodos([...todos, newTodo])
     setTodoText('')
   }
 
+  // Function to delete a todo based on its id
   function deleteTodo(id: number) {
+    // Filter out the todo with the given id and update the todos state
     const newTodos = todos.filter((todo) => todo.id !== id)
     setTodos(newTodos)
   }
 
+  // Function to toggle the completed status of a todo
   function toggleCompleted(id: number) {
+    // Map through the todos and toggle the completed status of the todo with the given id
     const newTodos = [...todos].map((todo) => {
       if (todo.id === id) {
         return {
           ...todo,
-          completed: !todo.completed,
+          completed: !todo.completed, // Toggle the completed status
         }
       } else {
         return todo
       }
     })
+    // Update the todos state with the modified list
     setTodos(newTodos)
   }
 
   return (
+    // Render a container for the to-do list
     <div className="toDoContainer">
       <div className="noteTitle">
         <h1>{props.heading}</h1>
@@ -52,33 +63,34 @@ function ToDoList(props: Props) {
 
       <div className="listItems">
         {todos.map((todo) => (
-          <>
-            <li key={todo.id}>
-              <button
-                className="deleteButton"
-                onClick={() => deleteTodo(todo.id)}
-              >
-                x
-              </button>
-              <button onClick={() => toggleCompleted(todo.id)}>
-                <span className={todo.completed ? 'toDoCompleted' : ''}>
-                  {todo.text}
-                </span>
-              </button>
-            </li>
-          </>
+          <li key={todo.id}>
+            <button
+              className="deleteButton"
+              onClick={() => deleteTodo(todo.id)}
+            >
+              x
+            </button>
+            <button onClick={() => toggleCompleted(todo.id)}>
+              <span className={todo.completed ? 'toDoCompleted' : ''}>
+                {todo.text}
+              </span>
+            </button>
+          </li>
         ))}
-        <input
-          className="inputBar"
-          type="text"
-          name="todoText"
-          placeholder={`Add to ${props.heading.toLowerCase()} list`}
-          value={todoText}
-          onChange={(e) => setTodoText(e.target.value)}
-        />
-        <button className="addButton" onClick={handleAddTodo}>
-          Add
-        </button>
+        <form onSubmit={(e) => handleAddTodo(e)}>
+          <input
+            className="inputBar"
+            type="text"
+            name="todoText"
+            placeholder={`Add to ${props.heading.toLowerCase()} list`}
+            value={todoText}
+            onChange={(e) => setTodoText(e.target.value)}
+            required
+          />
+          <button type="submit" className="addButton">
+            Add
+          </button>
+        </form>
       </div>
     </div>
   )
