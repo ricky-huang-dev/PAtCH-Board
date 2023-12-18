a9f4266a00f9573ebb79d26f69782dab07721d1e
gapson <hgapson@gmail.com>
2023-12-19 01:08:56 +1300

    making slide change randomly

--- client/components/Slideshow.tsx
+++ client/components/Slideshow.tsx
@@ -17,6 +17,10 @@ function SlideShow(props: Props) {
   }
   return (
     <div className="SlideshowContainer">
+      <div className="noteTitle">
+        <h1>{props.heading}</h1>
+      </div>
+
       <div className="slides">
         <button type="submit" className="slideButton" onClick={cycleImages}>
           <img className={'img'} alt="cats" src={images[imgIndex]} />
