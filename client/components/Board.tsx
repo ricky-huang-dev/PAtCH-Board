0900d086658efbf2e2a3869015efa6762d359fa2
gapson <hgapson@gmail.com>
2023-12-19 01:06:23 +1300

    adding background

--- client/components/Board.tsx
+++ client/components/Board.tsx
@@ -3,14 +3,12 @@ import StickyNote from './StickyNote'
 type Props = {
   bgStyle: string
 }
-
+// changing board background
 function Board({ bgStyle }: Props) {
   return (
     <div className={`board ${bgStyle}`}>
       <StickyNote color={'pink'} type="ToDoList" heading={'Groceries'} />
       <StickyNote color={'blue'} type="ToDoList" heading={'Projects'} />
-      <StickyNote color={'green'} type="ToDoList" heading={'Activities'} />
-      <StickyNote color={'yellow'} type="Slideshow" heading={'Cats'} />
     </div>
   )
 }
